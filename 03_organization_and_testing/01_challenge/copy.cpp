7d36b70b2712b8c59e86b121053e75b7df8c4189
arikai23 <tatsuya.arikai@woven-planet.global>
2023-12-27 17:25:53 +0900

    Submit challenge for grading

---
+++ 03_organization_and_testing/01_challenge/copy.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "temp_sensor.h"
+
+
+//float readTemperature() {
+//    return 25.6;
+//}
+
+int main() {
+   std::cout << "Temperature writing: " << std::endl;
+ return 0;
+}
---
+++ 03_organization_and_testing/01_challenge/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "temp_sensor.h"
+
+
+//float readTemperature() {
+//    return 25.5;
+//}
+
+int main() {
+    std::cout << "Temperature reading: " << readTemperature() << std::endl;
+    return 0;
+}
