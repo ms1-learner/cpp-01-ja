f68a997c17a25da4414090186c6fbeef7324bdba
arikai23 <tatsuya.arikai@woven-planet.global>
2024-01-18 17:49:37 +0900

    Submit challenge for grading

---
+++ 04_memory_and_error_handling/02_challenge/main.cpp
@@ -0,0 +1,25 @@
+//2つの整数型のポインタを引数にとり、これらのポインタが指す値を入れ替える関数 `swap_int` を作成してください。次に、main関数からこの関数を呼び出し、値が入れ替わっていることを確認します。
+
+
+#include <iostream>
+
+void swap( int& a, int& b )
+{
+    int tmp; 
+    tmp = a;
+    a = b;
+    b = tmp; 
+}
+
+
+
+
+int main() 
+{
+    int a = 10;
+    int b = 20;
+
+    std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
+   swap(a, b);
+    std::cout << "After swap: a = " << a << ", b = " << b << std::endl;
+}
